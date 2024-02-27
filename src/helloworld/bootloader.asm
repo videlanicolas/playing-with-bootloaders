342568c15480b3e1d145e6ad5892d1fb8f41f43a
videlanicolas <videlanicolas@gmail.com>
2024-02-27 12:10:59 +0000

    More typos.

--- src/helloworld/bootloader.asm
+++ src/helloworld/bootloader.asm
@@ -12,5 +12,5 @@ jmp $           ; Jump forever.
 HELLO_WORLD: db "Hello World!", 0   ; Just a blob of bytes that match the string, plus the NULL byte.
 
 ; spacing and signature
-times 510 - ($ - $$) db 0   ; This line basically adds 0x00 byte to fill up until 510 bytes.
-dw 0xaa55                   ; Magic number for BIOS to detect that this sector has a bootloader.
+times 510 - ($ - $$) db 0   ; This line basically adds 0x00 bytes to fill up until 510 bytes.
+dw 0xaa55                   ; Magic number for BIOS to detect that this sector is a bootloader.
