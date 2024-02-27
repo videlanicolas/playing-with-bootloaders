cddaa1370086b2ce0467f5e03036025c7afdc214
videlanicolas <videlanicolas@gmail.com>
2024-02-27 12:13:42 +0000

    More typos/

--- src/common/print_string.asm
+++ src/common/print_string.asm
@@ -5,9 +5,9 @@ print_string:
 
     print_string_loop:
 
-    cmp byte[bx], 0x00      ; Compare BL to the NULL byte.
+    cmp byte[bx], 0x00      ; Compare the low byte referenced by BX to the NULL byte.
     je print_string_end     ; If we found the NULL byte, end the loop.
-    mov al, byte[bx]        ; Move the low byte (BL) to AL, this will be printed out by the BIOS.
+    mov al, byte[bx]        ; Copy the byte at BX to AL, this will be printed out by the BIOS.
     int 0x10                ; https://en.wikipedia.org/wiki/INT_10H
     add bx, 1               ; Get the next 16 bit address from memory, i.e. the next char to print.
     jmp print_string_loop   ; Loop back and repeat.
