ca37b29d14c8ffe44783c9aed70326442dc172d7
videlanicolas <videlanicolas@gmail.com>
2024-02-29 12:59:28 +0000

    Fix typos.

--- src/common/print_hex.asm
+++ src/common/print_hex.asm
@@ -2,26 +2,26 @@
 ; Watch out, it's including print_string.asm!
 print_hex:
     pusha                       ; Save all registers to the stack.
-    mov bx, PRINT_HEX_STR+2     ; Get hte first char in the hex string, after '0x'.
+    mov bx, PRINT_HEX_STR+2     ; Get the first char in the hex string after '0x'.
     mov ax, 0xf000              ; The start mask, this will slide 'f' to the right.
-    mov [INPUT_VAR], cx         ; We'll reference this later, so we free CX.
-    mov cx, 12                  ; Number of bits to shift right in DX, this will be a sliding unmber.
+    mov [INPUT_VAR], cx         ; We'll reference this later, so we can free CX and use it.
+    mov cx, 12                  ; Number of bits to shift right in DX, this will be a sliding number.
 
 print_hex_loop:
     ; We need to manipulate the string in BX to change it 
-    ; to the correspondig ASCII byte of that same number.
+    ; to the corresponding ASCII byte of that same number.
     mov dx, [INPUT_VAR]         ; Get a fresh copy of the word to print.
     and dx, ax                  ; Apply the mask.
-    shr dx, cl                  ; Get rid of all the zeros.
+    shr dx, cl                  ; Move the bits that we care to the least significant bits.
     call print_hex_add_ascii    ; Convert the resulting byte to ASCII.
-    
+
     ; Once the correct ASCII byte was calculated, copy it to where BX is pointing.
     mov [bx], dx
 
-    ; The following is preparing all registers to the next iteration of this loop.
+    ; The following prepares all registers to the next iteration of the loop.
     inc bx                      ; Get the next address to print.
     shr ax, 4                   ; Get the next mask.
-    sub cl, 4                   ; Next time we'll only need to shift 4 bits less.
+    sub cl, 4                   ; Get the next bits to shift right.
     cmp ax, 0x0000              ; If this is zero then we're done.
     jne print_hex_loop
 
